b4aea04d525fbedc683cf7e82b7af61e76de908d
Collin Ballou <BlueEagle@users.noreply.github.com>
2020-06-18 22:39:53 -0600

    wip

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import NavBar from './NavBar'
 
 function App() {
   return (
     <div className="App">
-      <header>
-        
-      </header>
+      <Router>
+        <header>
+          <NavBar/>
+        </header>
+      </Router>
     </div>
   );
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,34 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
 
 const NavBar = (props) => {
   return (
-    <nav>
+    <StyledNav>
+      <div>
+        <StyledH1>Collin's Code</StyledH1>
+      </div>
       
-    </nav>
+      <div>
+        <Link to="/projects">Projects</Link>
+        <Link to="/blog">Blog</Link>
+        <Link to="/contact">Contact</Link>
+      </div>
+    </StyledNav>
   )
 }
 
 export default NavBar
+
+// Styled components
+const StyledNav = styled.nav`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  height: 4vh;
+  background: lightblue;
+  padding: 2%;
+`
+const StyledH1 = styled.h1`
+  margin: 0;
+`
