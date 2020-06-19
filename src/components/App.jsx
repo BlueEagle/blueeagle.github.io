f4db5d6b7917d574d67642a02224fb10e4a5b529
Collin Ballou <BlueEagle@users.noreply.github.com>
2020-06-18 23:35:51 -0600

    wip

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import styled from 'styled-components'
 import NavBar from './NavBar'
 
 function App() {
+  const [globalStyleValues, setGlobalStyleValues] = useState({
+    fontColor: 'black'
+  })
+  const { fontColor } = globalStyleValues
+
+  const GloballyStyledDiv = styled.div`
+    color: ${fontColor};
+
+    a {
+      color: ${fontColor};
+      text-decoration: none;
+    }
+  `
+
   return (
-    <div className="App">
+    <GloballyStyledDiv className="App">
       <Router>
         <header>
-          <NavBar/>
+          <NavBar style={globalStyleValues}/>
         </header>
       </Router>
-    </div>
+    </GloballyStyledDiv>
   );
 }
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,32 +3,55 @@ import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 
 const NavBar = (props) => {
+
+  // Styled components
+  const StyledNav = styled.nav`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    height: 8vh;
+    background: lightblue;
+    padding-left: 3%;
+    padding-right: 3%;
+  `
+  const StyledH1 = styled.h1`
+    margin-left: 0;
+  `
+  const RightNavDiv = styled.div`
+    display: flex;
+    flex-basis: 30%;
+    height: 100%;
+    justify-content: space-between;
+  `
+  // const ButtonDiv = styled.div`
+  //   box-sizing: border-box;
+  //   height: 100%;
+  //   background-color: red;
+  // `
+  const StyledLink = styled(Link)`
+    padding: 3%;
+    background-color: red;
+    margin: 0;
+  `
+
   return (
     <StyledNav>
       <div>
         <StyledH1>Collin's Code</StyledH1>
       </div>
       
-      <div>
-        <Link to="/projects">Projects</Link>
-        <Link to="/blog">Blog</Link>
-        <Link to="/contact">Contact</Link>
-      </div>
+      <RightNavDiv>
+        {/* // <Link to="/projects"><ButtonDiv>Projects</ButtonDiv></Link>
+        // <Link to="/blog"><ButtonDiv>Blog</ButtonDiv></Link>
+        // <Link to="/contact"><ButtonDiv>Contact</ButtonDiv></Link> */}
+
+        <StyledLink to="/projects">Projects</StyledLink>
+        <StyledLink to="/blog">Blog</StyledLink>
+        <StyledLink to="/contact">Contact</StyledLink>
+
+      </RightNavDiv>
     </StyledNav>
   )
 }
 
 export default NavBar
-
-// Styled components
-const StyledNav = styled.nav`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  height: 4vh;
-  background: lightblue;
-  padding: 2%;
-`
-const StyledH1 = styled.h1`
-  margin: 0;
-`
