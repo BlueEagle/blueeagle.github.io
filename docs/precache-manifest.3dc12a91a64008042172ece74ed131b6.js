e736ec8f78e08e937c3a21ab92ef9a83ae377d69
Collin Ballou <BlueEagle@users.noreply.github.com>
2020-06-19 15:39:01 -0600

    wip

---
+++ docs/precache-manifest.3dc12a91a64008042172ece74ed131b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc6ea1241181996cbcad3ced0fa15717",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eb962f194b5277702318",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "87f0318b45c122e580cd",
+    "url": "/static/js/2.6d233370.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6d233370.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb962f194b5277702318",
+    "url": "/static/js/main.6538542e.chunk.js"
+  },
+  {
+    "revision": "b901a15fadc1f3eb707d",
+    "url": "/static/js/runtime-main.4f1e7f78.js"
+  }
+]);
