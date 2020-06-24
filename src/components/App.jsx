03a96c504547afea3716a7366b11ee9db241ce02
Collin Ballou <BlueEagle@users.noreply.github.com>
2020-06-24 17:14:35 -0600

    removed code causing build problems

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import {BrowserRouter as Router} from 'react-router-dom'
 import styled from 'styled-components'
 import NavBar from './NavBar'
 import BioDiv from './BioDiv'
